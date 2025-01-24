99f2e0adaadf2637ce9ae2f9c157bc052d9b4169
janavarrob04 <janavarrobp@gmail.com>
2025-01-24 13:36:31 +0100

    Update uapp.py

--- uapp.py
+++ uapp.py
@@ -65,6 +65,10 @@ def configurar_driverb():
     options.add_experimental_option("useAutomationExtension", False)
     options.add_argument("start-maximized")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
+    if 'GOOGLE_CHROME_BIN' in os.environ:
+        options.binary_location = os.environ['GOOGLE_CHROME_BIN']
+
     driver = uc.Chrome(options=options)
     return driver
 
